e5cc13fa8f8e437ccd3e71cd2d6ddead86cdefef
Tu4ka <shchepanowska@gmail.com>
2022-09-08 19:38:40 +0700

    task-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
-function factorial(n) {
-  // ваш код...
+function factorial(n){
+  if (n <= 1) {
+      return 1;
+  }
+  let result = n;
+  for (let i = 1; i < n; ++i) {
+      result = result * i;
+  }
+  return result;
+
 }
